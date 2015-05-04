c40bfdda4a2baa109186828bd793c6b4c7956720
oscartcy <tang_co@yahoo.com.hk>
2015-05-04 23:46:39 +0800

    POC on rotating the globe

--- script.js
+++ script.js
@@ -22,6 +22,8 @@ svg.call(
         })
 );
 
+var countries;
+
 d3.json("/data/world-110m.json", function(error, world) {
     svg.append("path")
         .datum(topojson.feature(world, world.objects.land))
@@ -32,4 +34,51 @@ d3.json("/data/world-110m.json", function(error, world) {
         .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
         .attr("class", "boundary")
         .attr("d", path);
+
+    countries = topojson.feature(world, world.objects.countries).features;
+});
+
+var geocode = function (q) {
+    return $.ajax("http://nominatim.openstreetmap.org/search/", {
+        data: {
+            q: q,
+            format: "json"
+        }
+    }).then(function(data) {
+        if (!data || !data[0]) {
+            throw new Error("Geocoding '" + q + "' failed.");
+        }
+        // return {
+        //     lat: parseFloat(data[0].lat, 10),
+        //     lon: parseFloat(data[0].lon, 10)
+        // };
+        // return [parseFloat(data[0].lat, 10), parseFloat(data[0].lon, 10)];
+        return { "type": "Point", "coordinates": [parseFloat(data[0].lon, 10), parseFloat(data[0].lat, 10)] };
+    });
+};
+
+geocode('hong kong').then(function(result) {
+    console.log('geocoding: ', result);
+
+    // var p = d3.geo.centroid(result);
+    // // var r = d3.interpolate(projection.rotate(), [-p[0], -p[1]]);
+    // projection.rotate(result);
+    // svg.selectAll("path").attr("d", path);
+
+    d3.transition()
+        .duration(1250)
+        .tween("rotate", function() {
+            var p = d3.geo.centroid(result),
+            r = d3.interpolate(projection.rotate(), [-p[0], -p[1]]);
+            console.log(countries[0]);
+            return function(t) {
+                projection.rotate(r(t));
+                svg.selectAll("path").attr("d", path);
+            };
+        });
+
+    // svg.call(
+    //     d3.geo.zoom().projection(projection)
+    //         .center(result)
+    // );
 });
