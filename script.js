7cdabf026249e475518c60eef5d451006f3fca01
oscartcy <tang_co@yahoo.com.hk>
2015-05-04 22:08:51 +0800

    init commit

---
+++ script.js
@@ -0,0 +1,30 @@
+var width = 960,
+    height = 500;
+
+var dragFactor = 1;
+
+var projection = d3.geo.orthographic()
+    .scale(250)
+    .translate([width / 2, height / 2])
+    .clipAngle(90);
+
+var path = d3.geo.path()
+    .projection(projection);
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+svg.call(
+    d3.geo.zoom().projection(projection)
+        .on("zoom", function() {
+            svg.selectAll("path").attr("d", path);
+        })
+);
+
+d3.json("/data/world-110m.json", function(error, world) {
+    svg.append("path")
+        .datum(topojson.feature(world, world.objects.land))
+        .attr("class", "land")
+        .attr("d", path);
+});
